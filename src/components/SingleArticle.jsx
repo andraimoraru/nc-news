345db9369d796f91c6dd12d7eb9e026afd14cd2a
Andra Moraru <andraimoraru>
2023-09-06 11:28:33 +0100

    a minor change in my SingleArticle file

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
-import { getArticleById, getCommentsByArticleId } from "../utils/api";
+import { getArticleById } from "../utils/api";
 import ArticleCard from "./ArticleCard";
 import CommentCard from "./CommentCard";
 
