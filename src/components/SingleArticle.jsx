19c4b509f53855769c0bfa18e35fb16da77bf7c5
Andra Moraru <andraimoraru>
2023-09-07 12:25:16 +0100

    this commit will add task 7

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -3,15 +3,14 @@ import { useParams } from "react-router-dom";
 import { getArticleById } from "../utils/api";
 import ArticleCard from "./ArticleCard";
 import CommentCard from "./CommentCard";
+import VoteCard from "./VoteCard"
 
-export default function SingleArticle({article}) {
+export default function SingleArticle() {
 
     const [singleArticle, setSingleArticle]= useState({});
     const [isLoading, setIsLoading]= useState(true);  
     const {article_id} = useParams();
-
-   
-
+  
     useEffect(() => {
         setIsLoading(true);
         getArticleById(article_id)
@@ -31,6 +30,9 @@ export default function SingleArticle({article}) {
         <ArticleCard 
         key={singleArticle.article_id} 
         article={singleArticle}       
+        />
+        <VoteCard
+        article = {singleArticle}
         />
         <h2>  Comments  </h2>
         <CommentCard
---
+++ src/components/VoteCard.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { decreaseVotesByArticleId, getArticleById, increaseVotesByArticleId } from "../utils/api"
+import { useParams } from "react-router-dom";
+
+
+export default function VoteCard({article}) {
+
+    const [defaultVotes, setDefaultVotes] = useState(0);
+    const {article_id} = useParams();
+   
+    useEffect(() => {
+        getArticleById(article_id)
+        .then((articleFromApi) => {
+            setDefaultVotes(articleFromApi.votes)
+        })
+    }, []);
+
+
+    const IncreaseVotes = (article_id) => {
+        increaseVotesByArticleId(article_id);
+        const updatedIncreasedVotes = defaultVotes + 1;
+        setDefaultVotes(updatedIncreasedVotes);
+    }
+
+    const DecreaseVotes =(article_id) => {
+        decreaseVotesByArticleId(article_id);
+        const updatedDecreasedVotes = defaultVotes -1;
+        setDefaultVotes(updatedDecreasedVotes);
+
+    }
+
+    return ( 
+    <div className="votes_view">
+        <div> 
+            <button className="vote_button" onClick={() => {
+                IncreaseVotes(article_id);
+            }}>
+                 + 1 
+            </button>
+              Votes  {defaultVotes} 
+             <button className="vote_button" onClick={()=> {
+                DecreaseVotes(article_id);
+             }}>
+                 - 1 
+            </button>
+        </div>
+    </div>
+)
+}
