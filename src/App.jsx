54ba6e6417ea80f3f8142e5ef8567f64f9080cef
Andra Moraru <andraimoraru>
2023-10-08 00:09:29 +0100

    this commit will add the home path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
 import NavBar from './components/NavBar'
 import ArticlesList from './components/ArticlesList'
 import SingleArticle from './components/SingleArticle'
@@ -16,6 +15,7 @@ function App() {
     <NavBar />
     <div className='container'>
       <Routes>
+        <Route path = "/" element={<ArticlesList/>}/>
         <Route path = "/articles" element={<ArticlesList/>}/>
         <Route path = "/articles/:article_id" element={<SingleArticle/>}/>
         <Route path = "/users" element={<UsersList/>}/>
